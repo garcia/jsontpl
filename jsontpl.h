4ab35ac9844c6079d1e89885f59f06197bd0536e
Grant Garcia <github@grantgarcia.org>
2014-02-06 17:00:32 -0700

    remove unused macro

--- jsontpl.h
+++ jsontpl.h
@@ -3,7 +3,6 @@
 
 #define JSONTPL_JSON_ERROR "invalid JSON: %s (line %d, column %d)\n"
 #define JSONTPL_FILTER_ERROR "filter '%s' requires %s"
-#define JSONTPL_DUMPS_FLAGS (JSON_ENSURE_ASCII | JSON_ENCODE_ANY)
 
 int jsontpl_string(char *json, char *template, char **output);
 int jsontpl_file(char *json_filename, char *template_filename, char **output);
